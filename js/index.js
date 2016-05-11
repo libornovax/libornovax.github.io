eaffd46fe5ca5cd093ef8479debecc149d35bf2a
Libor Novak <libornovak@icloud.com>
2016-05-10 21:28:21 -0700

    Tiles reorganize according to browser window width

--- js/index.js
+++ js/index.js
@@ -21,10 +21,51 @@ $(document).ready(function () {
 	$('.tile').on('mouseleave', function () {
 		// Flip back
 		var card = $(this);
-		setTimeout(function () {
-			card.removeClass('flipped');
-		}, 1000);
+		card.removeClass('flipped');
 	});
 
 });
+
+
+// ------------------------  HANDLE THE TILE TABLE  ------------------------ //
+var tiles;
+
+var organizeTiles = function () {
+	$('#tile-table').children().detach();
+
+	var per_row = 3;
+	if ($(window).width() < 750)
+	{
+		per_row = 2;
+	}
+	if ($(window).width() < 450)
+	{
+		per_row = 1;
+	}
+
+	var row = $('<div class="row"></div>');
+	var current_row = 0;
+	tiles.each(function () {
+		var tl = $(this);
+		tl.css('width', 100/per_row + '%');
+		row.append($(this));
+		current_row += 1;
+
+		if (current_row === per_row)
+		{
+			$('#tile-table').append(row);
+			row = $('<div class="row"></div>');
+			current_row = 0;
+		}
+	});
+};
+
+$(window).load(function () {
+	tiles = $('#tile-table').children();
+	tiles.detach();
+	organizeTiles();
+
+	$(window).resize(organizeTiles);
+});
+
 
